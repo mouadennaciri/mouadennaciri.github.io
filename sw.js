1679602c086f69732ff5d1327c0d564af44d7480
Mouad Ennaciri <mouadennaciri@192.168.1.2>
2025-09-20 18:24:55 +0100

    pwa part 4

--- sw.js
+++ sw.js
@@ -1,17 +1,42 @@
-const CACHE_NAME = 'mouad-ennaciri-v1';
+const CACHE_NAME = 'mouad-ennaciri-v2';
 const urlsToCache = [
-  './',
-  './index.html',
-  './css/main.css',
-  './css/default.css',
-  './js/hl.js',
-  './manifest.json'
+  '/',
+  '/index.html',
+  '/css/main.css',
+  '/css/default.css',
+  '/js/hl.js',
+  '/manifest.json'
 ];
 
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+      .then(cache => {
+        console.log('Caching files');
+        return cache.addAll(urlsToCache);
+      })
+      .then(() => {
+        console.log('Service Worker installation complete');
+        return self.skipWaiting();
+      })
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            console.log('Deleting old cache:', cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    }).then(() => {
+      console.log('Service Worker activation complete');
+      return self.clients.claim();
+    })
   );
 });
 
@@ -20,10 +45,25 @@ self.addEventListener('fetch', event => {
     caches.match(event.request)
       .then(response => {
         if (response) {
+          console.log('Serving from cache:', event.request.url);
           return response;
         }
-        return fetch(event.request);
-      }
-    )
+        console.log('Fetching from network:', event.request.url);
+        return fetch(event.request).then(response => {
+          if (!response || response.status !== 200 || response.type !== 'basic') {
+            return response;
+          }
+          const responseToCache = response.clone();
+          caches.open(CACHE_NAME)
+            .then(cache => {
+              cache.put(event.request, responseToCache);
+            });
+          return response;
+        });
+      }).catch(() => {
+        if (event.request.destination === 'document') {
+          return caches.match('/index.html');
+        }
+      })
   );
 });
